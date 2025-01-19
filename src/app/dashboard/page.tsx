264a0b8a5bb8f4cd0cdb0eda86c265089e774c7c
Hemanshu-Upadhyay <upadhyayhri@gmail.com>
2025-01-19 14:07:53 +0530

    SEO Optimisation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
-"use client";
-
 import React from "react";
 import DashboardPage from "./ui/page";
 import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Manage Employee Data",
+};
 
 const page = () => {
   return <DashboardPage />;
